c6218e1483e8860d600f88ad6625a69aa5e5023e
Trinix22 <aprendizdesherek22@gmail.com>
2025-11-21 14:41:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
         return 
     
 
-    # TODO: llamar a la función triangulo_simetrico con los parámetros m y s
-triangulo_simetrico(m, s)
+    # TODO: llamar a la función triangulo_simetrico con los parámetros m y s 
+    triangulo_simetrico(m, s)
 if __name__ == "__main__":
     main()
